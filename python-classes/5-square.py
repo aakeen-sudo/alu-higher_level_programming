516de3e1081e0b7f606ce171440e409870d521fa
Adit <a.akeen@alustudent.com>
2026-07-12 13:20:43 -0700

    Add 5-square.py

---
+++ python-classes/5-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Define a class Square with a size property, area, and printing."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Get/set the current size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the current area of the square."""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Print the square with the # character.
+
+        An empty line is printed if size is 0.
+        """
+        if self.__size == 0:
+            print()
+        else:
+            for row in range(self.__size):
+                print("#" * self.__size)
