5b36234afa13628e68e5a215ffcf5d1a60d6ff24
Adit <a.akeen@alustudent.com>
2026-07-12 12:39:53 -0700

    Add 3-infinite_add.py

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""Print the addition of all command-line arguments."""
 if __name__ == "__main__":
     from sys import argv
-    result = 0
+
+    total = 0
     for i in range(1, len(argv)):
-        result += int(argv[i])
-    print("{}".format(result))
+        total += int(argv[i])
+    print(total)
