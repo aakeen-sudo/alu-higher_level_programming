f55df5e579eaef138368fc9b2e6ae39fe759702c
Adit <a.akeen@alustudent.com>
2026-07-12 09:56:29 -0700

    Fix 8-uppercase.py: 2 prints only

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
     for c in str:
-        if ord(c) >= ord('a') and ord(c) <= ord('z'):
-            print("{}".format(chr(ord(c) - 32)), end="")
-        else:
-            print("{}".format(c), end="")
+        print("{}".format(chr(ord(c) - 32) if ord(c) >= 97 and
+                          ord(c) <= 122 else c), end="")
     print("")
