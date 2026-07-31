5009013b4b96c0ab73b163762970f598a796c024
aakeen-sudo <a.akeen@alustudent.com>
2026-07-31 13:59:18 +0100

    Add 4-inherits_from.py

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module that defines a function to check if an object is an instance
+of a class that inherited (directly or indirectly) from a specified
+class."""
+
+
+def inherits_from(obj, a_class):
+    """Return True if obj is an instance of a class that inherited,
+    directly or indirectly, from a_class."""
+    return isinstance(obj, a_class) and type(obj) != a_class
