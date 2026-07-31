3a80a58ea1feaa5af2bdb9ede0fdb30aafef0183
aakeen-sudo <a.akeen@alustudent.com>
2026-07-31 13:58:16 +0100

    Add 3-is_kind_of_class.py

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module that defines a function to check if an object is an instance
+of, or inherited from, a specified class."""
+
+
+def is_kind_of_class(obj, a_class):
+    """Return True if obj is an instance of a_class or a subclass of it."""
+    return isinstance(obj, a_class)
