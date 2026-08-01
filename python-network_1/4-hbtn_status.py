89963f0c147681059a875f880cd767d722e09156
aakeen-sudo <a.akeen@alustudent.com>
2026-08-01 16:56:09 +0100

    Add 4-hbtn_status.py

---
+++ python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Fetches https://alu-intranet.hbtn.io/status using requests"""
+import requests
+
+if __name__ == "__main__":
+    response = requests.get("https://alu-intranet.hbtn.io/status")
+    body = response.text
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
