a1e27b0d86377596b5cb0d0ef4ea8cb2f96b4818
aakeen-sudo <a.akeen@alustudent.com>
2026-08-01 13:29:33 +0100

    Add task 7: cities table

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+-- creates database hbtn_0d_usa (if needed) and table cities with foreign key to states
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+    id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id)
+);
